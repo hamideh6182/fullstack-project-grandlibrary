ac3413911b86a5f1d18a1950f37eb30f58c0deaa
hamideh <hamideh_h61@yahoo.com>
2023-03-19 09:22:15 +0100

    add annotation @Document and @Id for Book record

--- backend/src/main/java/com/github/hamideh6182/model/Book.java
+++ backend/src/main/java/com/github/hamideh6182/model/Book.java
@@ -1,6 +1,11 @@
 package com.github.hamideh6182.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("books")
 public record Book(
+        @Id
         String id,
         String title,
         String author,
