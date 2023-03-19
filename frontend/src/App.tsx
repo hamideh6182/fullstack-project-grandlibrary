f69ff61ab9bf5c50186eb294e2f6b57089d9e567
hamideh <hamideh_h61@yahoo.com>
2023-03-19 14:24:43 +0100

    add route to BooksGallery "/Books" to show all books

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import BooksGallery from "./pages/BooksGallery";
+import useBooks from "./hooks/useBooks";
 
 function App() {
+  const {books} = useBooks()
   return (
-    <div className="App">
-
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path={"/Books"} element={<BooksGallery books={books}/>}/>
+        </Routes>
+      </div>
   );
 }
 
