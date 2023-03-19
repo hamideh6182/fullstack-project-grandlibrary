af70b2cdb542759ce5550975af9eef18ffaf9bd2
hamideh <hamideh_h61@yahoo.com>
2023-03-19 09:47:12 +0100

    create controller package and BookController class and getAllBooks method with Endpoint "/api/books"

---
+++ backend/src/main/java/com/github/hamideh6182/controller/BookController.java
@@ -0,0 +1,22 @@
+package com.github.hamideh6182.controller;
+
+import com.github.hamideh6182.model.Book;
+import com.github.hamideh6182.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    public final BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+}
