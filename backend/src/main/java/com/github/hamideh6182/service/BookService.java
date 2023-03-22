cf3a0787f10578699404cba3df83dd43709a8783
hamideh <hamideh_h61@yahoo.com>
2023-03-22 20:07:30 +0100

    Change addBook method and add MultipartFile as parameters

--- backend/src/main/java/com/github/hamideh6182/service/BookService.java
+++ backend/src/main/java/com/github/hamideh6182/service/BookService.java
@@ -5,7 +5,10 @@ import com.github.hamideh6182.model.BookRequest;
 import com.github.hamideh6182.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.List;
 
 @Service
@@ -13,14 +16,25 @@ import java.util.List;
 public class BookService {
     private final BookRepository bookRepository;
     private final IdService idService;
+    private final PhotoService photoService;
 
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
     }
 
-    public Book addBook(BookRequest bookRequest) {
+    public Book addBook(BookRequest bookRequest, MultipartFile photo) {
         if (bookRequest.title() == null) {
             throw new IllegalArgumentException("missing title");
+        }
+        String photoUri;
+        if (photo != null) {
+            try {
+                photoUri = photoService.uploadPhoto(photo);
+            } catch (IOException e) {
+                throw new InputMismatchException("The photo upload didn't work: " + e.getMessage());
+            }
+        } else {
+            photoUri = null;
         }
         String id = idService.generateId();
         Book newBook = new Book(
@@ -31,7 +45,7 @@ public class BookService {
                 bookRequest.copies(),
                 bookRequest.copies(),
                 bookRequest.category(),
-                bookRequest.img()
+                photoUri
         );
         return bookRepository.save(newBook);
     }
