cabd79481150806cf104bf8491c5e70a10046fb4
hamideh <hamideh_h61@yahoo.com>
2023-03-23 18:44:34 +0100

    add delete Button in BookDetails.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,13 +7,13 @@ import AddBook from "./pages/AddBook";
 import BookDetails from "./pages/BookDetails";
 
 function App() {
-    const {books, postNewBook} = useBooks()
+    const {books, postNewBook, deleteBook} = useBooks()
   return (
       <div className="App">
           <Routes>
               <Route path={"/Books"} element={<BooksGallery books={books}/>}/>
               <Route path={"/Books/add"} element={<AddBook onAddBook={postNewBook}/>}/>
-              <Route path={"/Books/:id"} element={<BookDetails books={books}/>}/>
+              <Route path={"/Books/:id"} element={<BookDetails books={books} deleteBook={deleteBook}/>}/>
           </Routes>
       </div>
   );
