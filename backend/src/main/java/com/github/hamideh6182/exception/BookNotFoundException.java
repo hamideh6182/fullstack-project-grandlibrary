74cf490f39fca4c75ff298eaf7f8424dec35cd5d
hamideh <hamideh_h61@yahoo.com>
2023-03-23 19:27:32 +0100

    Remove empty line and correct the message

--- backend/src/main/java/com/github/hamideh6182/exception/BookNotFoundException.java
+++ backend/src/main/java/com/github/hamideh6182/exception/BookNotFoundException.java
@@ -2,12 +2,10 @@ package com.github.hamideh6182.exception;
 
 public class BookNotFoundException extends RuntimeException {
     public BookNotFoundException() {
-        super("Current Task not found");
-
+        super("Current Book not found");
     }
 
     public BookNotFoundException(String message) {
         super(message);
-
     }
 }
