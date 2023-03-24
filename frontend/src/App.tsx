c107b05e39341760cedff5da0aa6743b48b7531f
hamideh <hamideh_h61@yahoo.com>
2023-03-24 14:43:20 +0100

    add updateBookIncrease method in useBooks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,13 +7,14 @@ import AddBook from "./pages/AddBook";
 import BookDetails from "./pages/BookDetails";
 
 function App() {
-    const {books, postNewBook, deleteBook} = useBooks()
+    const {books, postNewBook, deleteBook, updateBookIncrease} = useBooks()
   return (
       <div className="App">
           <Routes>
               <Route path={"/Books"} element={<BooksGallery books={books}/>}/>
               <Route path={"/Books/add"} element={<AddBook onAddBook={postNewBook}/>}/>
-              <Route path={"/Books/:id"} element={<BookDetails books={books} deleteBook={deleteBook}/>}/>
+              <Route path={"/Books/:id"} element={<BookDetails books={books} deleteBook={deleteBook}
+                                                               updateBookIncrease={updateBookIncrease}/>}/>
           </Routes>
       </div>
   );
