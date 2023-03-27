bba8a52b1075d5ce1cbe87af5ead9ef83873a5d5
hamideh <hamideh_h61@yahoo.com>
2023-03-27 18:08:30 +0200

    Change some code user authentication

---
+++ backend/src/main/java/com/github/hamideh6182/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.github.hamideh6182.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    private static final String ADMIN = "ADMIN";
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName(null);
+
+        return http
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(requestHandler))
+                .httpBasic()
+                .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase()))
+                .and()
+                .sessionManagement(config ->
+                        config.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET, "/api/csrf").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+                .requestMatchers(HttpMethod.POST, "api/books").hasRole(ADMIN)
+                .requestMatchers(HttpMethod.DELETE, "api/books/{id}").hasRole(ADMIN)
+                .requestMatchers(HttpMethod.PUT, "api/books/quantity/increase/{id}").hasRole(ADMIN)
+                .requestMatchers(HttpMethod.PUT, "api/books/quantity/decrease/{id}").hasRole(ADMIN)
+                .requestMatchers("/api/**").authenticated()
+                .anyRequest().permitAll().and()
+                .build();
+    }
+}
