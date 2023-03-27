8f427cee43fff803dcb4b140b16bcad5e4f7c3ee
hamideh <hamideh_h61@yahoo.com>
2023-03-27 17:06:00 +0200

    Add MongoUserRepository class in backend

---
+++ backend/src/main/java/com/github/hamideh6182/repository/MongoUserRepository.java
@@ -0,0 +1,12 @@
+package com.github.hamideh6182.repository;
+
+import com.github.hamideh6182.model.MongoUser;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface MongoUserRepository extends MongoRepository<MongoUser, String> {
+    Optional<MongoUser> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
