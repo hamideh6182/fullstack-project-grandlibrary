8b92ae45784ca5f21ae5d4d36754dd37f69980fa
hamideh <hamideh_h61@yahoo.com>
2023-03-27 17:51:26 +0200

    Delete some code

--- backend/src/main/java/com/github/hamideh6182/controller/MongoUserController.java
+++ backend/src/main/java/com/github/hamideh6182/controller/MongoUserController.java
@@ -13,24 +13,12 @@ import java.security.Principal;
 @RequestMapping("/api/users")
 public class MongoUserController {
     private final MongoUserDetailsService mongoUserDetailsService;
-
     @PostMapping
     public MongoUserResponse signup(@RequestBody MongoUserRequest user) {
         return mongoUserDetailsService.signup(user);
     }
-
-    @PostMapping("/login")
-    public MongoUserResponse login(Principal principal) {
-        return getMe(principal);
-    }
-
     @GetMapping("/me")
     public MongoUserResponse getMe(Principal principal) {
         return mongoUserDetailsService.getMe(principal);
-    }
-
-    @PostMapping("/logout")
-    public void logout() {
-        // this method exists only to define the endpoint!
     }
 }
