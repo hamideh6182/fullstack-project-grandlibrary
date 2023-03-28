d7642ce823f88a6f41305937da087d37dae89d7b
hamideh <hamideh_h61@yahoo.com>
2023-03-28 16:01:43 +0200

    add annotation @Document @Id

--- backend/src/main/java/com/github/hamideh6182/model/MongoUser.java
+++ backend/src/main/java/com/github/hamideh6182/model/MongoUser.java
@@ -1,6 +1,11 @@
 package com.github.hamideh6182.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("users")
 public record MongoUser(
+        @Id
         String id,
         String username,
         String password,
