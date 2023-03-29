42d7c3140c01f4b918df7bf6110e4dfa32cb5698
hamideh <hamideh_h61@yahoo.com>
2023-03-29 12:10:28 +0200

    Add axios interceptors for sending XSRF-TOKEN

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,16 +6,27 @@ import useBooks from "./hooks/useBooks";
 import AddBook from "./pages/AddBook";
 import BookDetails from "./pages/BookDetails";
 import Home from "./pages/Home";
+import axios from "axios";
+import Cookies from "js-cookie";
+
+axios.interceptors.request.use(function (config) {
+    return fetch("/api/csrf").then(() => {
+        config.headers["X-XSRF-TOKEN"] = Cookies.get("XSRF-TOKEN");
+        return config;
+    });
+}, function (error) {
+    return Promise.reject(error);
+});
 
 function App() {
     const {books, postNewBook, deleteBook, updateBookIncrease, updateBookDecrease} = useBooks()
-  return (
-      <div className="App">
-          <Routes>
-              <Route path={"/books"} element={<BooksGallery books={books}/>}/>
-              <Route path={"/books/add"} element={<AddBook onAddBook={postNewBook}/>}/>
-              <Route path={"/books/:id"} element={<BookDetails books={books} deleteBook={deleteBook}
-                                                               updateBookIncrease={updateBookIncrease}
+    return (
+        <div className="App">
+            <Routes>
+                <Route path={"/books"} element={<BooksGallery books={books}/>}/>
+                <Route path={"/books/add"} element={<AddBook onAddBook={postNewBook}/>}/>
+                <Route path={"/books/:id"} element={<BookDetails books={books} deleteBook={deleteBook}
+                                                                 updateBookIncrease={updateBookIncrease}
                                                                updateBookDecrease={updateBookDecrease}/>}/>
               <Route path={"/"} element={<Home/>}/>
           </Routes>
