663b4296e3bddb5e2d294e24911f3766d8520df4
hamideh <hamideh_h61@yahoo.com>
2023-03-29 14:21:52 +0200

    Move h1 and import CSS file

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,11 +1,12 @@
 import Layout from "../components/Layout";
 import SignForm from "../components/SignForm";
+import "./SignUp.css"
 
 export default function SignUp() {
     return (
         <Layout>
+            <h1 className={"h1-sign-up"}>Sign <span>Up</span></h1>
             <div style={{padding: "5rem 0"}}>
-                <h1>Sign Up</h1>
                 <SignForm action={"sign-up"}/>
             </div>
         </Layout>
