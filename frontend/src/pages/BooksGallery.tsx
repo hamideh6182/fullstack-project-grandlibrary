e6f2820a97a43c80b3cb136597c13dc15aaece86
hamideh <hamideh_h61@yahoo.com>
2023-04-12 12:45:40 +0200

    Add pagination in gallery

--- frontend/src/pages/BooksGallery.tsx
+++ frontend/src/pages/BooksGallery.tsx
@@ -2,7 +2,7 @@ import {Book} from "../models/Book";
 import BookCard from "../components/BookCard";
 import "./BooksGallery.css"
 import Layout from "../components/Layout";
-import {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 
 type Props = {
     books: Book[]
@@ -10,23 +10,44 @@ type Props = {
 export default function BooksGallery(props: Props) {
     const [searchTerm, setSearchTerm] = useState("");
     const [selectedCategory, setSelectedCategory] = useState("");
+    const [currentPage, setCurrentPage] = useState(0);
+    const booksPerPage = 2;
 
-    const bookCards = props.books
+    const filteredBooks = props.books
         .filter(
             (book) =>
                 book.title.toLowerCase().includes(searchTerm.toLowerCase()) &&
                 (selectedCategory === "" || book.category === selectedCategory)
-        )
+        );
+
+    const totalPages = Math.ceil(filteredBooks.length / booksPerPage);
+
+    const bookCards = filteredBooks
+        .slice(currentPage * booksPerPage, (currentPage + 1) * booksPerPage)
         .map((book: Book) => {
             return <BookCard book={book} key={book.id}/>;
         });
 
     function handleSearchInput(event: ChangeEvent<HTMLInputElement>) {
         setSearchTerm(event.target.value);
+        setCurrentPage(0);
     }
 
     function handleCategorySelect(event: ChangeEvent<HTMLSelectElement>) {
         setSelectedCategory(event.target.value);
+        setCurrentPage(0);
+    }
+
+    function handlePrevPage() {
+        if (currentPage > 0) {
+            setCurrentPage(currentPage - 1);
+        }
+    }
+
+    function handleNextPage() {
+        if (currentPage < totalPages - 1) {
+            setCurrentPage(currentPage + 1);
+        }
     }
 
     return (
@@ -47,7 +68,23 @@ export default function BooksGallery(props: Props) {
                         <option value={"programming"}>Programming</option>
                     </select>
                 </div>
-                <div>{bookCards.length > 0 ? bookCards : "Can't find what you are looking for?"}</div>
+                <div>{bookCards.length > 0
+                    ? bookCards
+                    : "Can't find what you are looking for?"}
+                </div>
+                <div>
+                    <span className={"book-gallery-span"}>
+                        Page {currentPage + 1} of {totalPages}
+                    </span>
+                </div>
+                <div className={"pagination"}>
+                    <button onClick={handlePrevPage} disabled={currentPage === 0}>
+                        Prev
+                    </button>
+                    <button onClick={handleNextPage} disabled={currentPage === totalPages - 1}>
+                        Next
+                    </button>
+                </div>
             </div>
         </Layout>
     );
